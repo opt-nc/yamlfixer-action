2864c7ebba594acbf41df7f5da3d670c24999962
Michèle BARRE <michele.barre@opt.nc>
2022-01-19 15:16:49 +1100

    fix repo name

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ if [ $result -ne 0 ] ; then
   echo "INFO : create a new branch with corrections." ;
   cd /github/workspace/app/
   branch_name=$(git branch --show-current)
-  repository_name=$(git config --get remote.origin.url | sed -e 's/^git@.*:\([[:graph:]]*\).git/\1/')
+  repository_name=$(basename $(git remote get-url origin) .git)
   echo $repository_name
   git config --global user.email "toto@opt.nc"
   git config --global user.name "Toto"
@@ -16,7 +16,7 @@ if [ $result -ne 0 ] ; then
   git add --all
   git commit -m 'yamlfixer patch proposition'
   echo "INFO : create a pull request." ;
-  curl  -v -X POST -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$repository_name/pulls \
+  curl  -v -X POST -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/opt-nc/$repository_name/pulls \
   -d '{"head":"yamlfixer_patch","base":'$branch_name'}'
 else
   echo "INFO : all input files either are skipped or successfully pass yamllint strict mode." ;
