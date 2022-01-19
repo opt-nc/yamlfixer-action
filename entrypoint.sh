32b6aad02b765001052445f5e5ad8c889b22dd0c
Michèle BARRE <michele.barre@opt.nc>
2022-01-19 14:52:32 +1100

    add verbose to curl

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,21 +5,17 @@
 result=$?
 if [ $result -ne 0 ] ; then
   echo "WARN: all input files didn't pass successfully yamllint strict mode." ;
+  echo "INFO : create a new branch with corrections." ;
+  cd /github/workspace/app/
   branch_name=$(git branch --show-current)
   repository_name=$(basename `git rev-parse --show-toplevel`)
-  pwd
-  ls
-  pwd
-  cd /github/workspace/app/
-  ls
-  echo "INFO : create a new branch with corrections." ;
   git config --global user.email "toto@opt.nc"
   git config --global user.name "Toto"
   git checkout -b yamlfixer_patch
   git add --all
   git commit -m 'yamlfixer patch proposition'
   echo "INFO : create a pull request." ;
-  curl  -X POST -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/opt-nc/$repository_name/pulls \
+  curl  -v -X POST -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/opt-nc/$repository_name/pulls \
   -d '{"head":"yamlfixer_patch","base":'$branch_name'}'
 else
   echo "INFO : all input files either are skipped or successfully pass yamllint strict mode." ;
