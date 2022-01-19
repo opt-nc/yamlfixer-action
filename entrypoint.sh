b550c5db4a14005b9d7ea159ad8b0ce4fb567c5d
Michèle BARRE <michele.barre@opt.nc>
2022-01-19 15:23:17 +1100

    fix curl command

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,8 +16,7 @@ if [ $result -ne 0 ] ; then
   git add --all
   git commit -m 'yamlfixer patch proposition'
   echo "INFO : create a pull request." ;
-  curl  -v -X POST -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/opt-nc/$repository_name/pulls \
-  -d '{"head":"yamlfixer_patch","base":'$branch_name'}'
+  curl  -v -X POST -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/opt-nc/$repository_name/pulls -d '{"head":"yamlfixer_patch","base":"'$branch_name'"}'
 else
   echo "INFO : all input files either are skipped or successfully pass yamllint strict mode." ;
 fi ;
