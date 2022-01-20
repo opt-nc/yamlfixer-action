0a0410a1719d3337b6f93db61714e1277a4f5537
Michèle BARRE <michele.barre@opt.nc>
2022-01-20 13:53:39 +1100

    add timestamp to branch name

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,15 +9,16 @@ if [ $result -ne 0 ] ; then
   cd /github/workspace/app/
   branch_name=$(git branch --show-current)
   repository_name=$(basename $(git remote get-url origin) .git)
+  current_timestamp=$(($(date +%s)))
   echo $repository_name
   git config --global user.email $USER"@opt.nc"
   git config --global user.name $USER
-  git checkout -b yamlfixer/$branch_name
+  git checkout -b yamlfixer/$branch_name/current_timestamp
   git add --all
   git commit -m 'Yamlfixer : fix yaml files '$YAML_FILE
-  git push origin yamlfixer/$branch_name
+  git push origin yamlfixer/$branch_name/current_timestamp
   echo "INFO : create a pull request." ;
-  curl  -u $USER:$TOKEN -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/opt-nc/$repository_name/pulls -d '{"head":"'yamlfixer/$branch_name'","base":"'$branch_name'", "title":"Fix yaml files '$YAML_FILE'"}'
+  curl  -u $USER:$TOKEN -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/opt-nc/$repository_name/pulls -d '{"head":"'yamlfixer/$branch_name/current_timestamp'","base":"'$branch_name'", "title":"Fix yaml files '$YAML_FILE'"}'
 else
   echo "INFO : all input files either are skipped or successfully pass yamllint strict mode." ;
 fi ;
