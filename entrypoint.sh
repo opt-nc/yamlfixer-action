85f973c81230426010db8943dc73aad774443c67
Michèle BARRE <michele.barre@opt.nc>
2022-01-21 11:16:58 +1100

    use optnc docker yamlfixer docker image

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,12 +13,12 @@ if [ $result -ne 0 ] ; then
   echo $repository_name
   git config --global user.email $USER"@opt.nc"
   git config --global user.name $USER
-  git checkout -b yamlfixer/$branch_name/$current_timestamp
+  git checkout -b yamlfixer/patch/$branch_name/$current_timestamp
   git add --all
   git commit -m 'Yamlfixer : fix yaml files '$YAML_FILE
-  git push origin yamlfixer/$branch_name/$current_timestamp
+  git push origin yamlfixer/patch/$branch_name/$current_timestamp
   echo "INFO : create a pull request." ;
-  curl  -u $USER:$TOKEN -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/opt-nc/$repository_name/pulls -d '{"head":"'yamlfixer/$branch_name/$current_timestamp'","base":"'$branch_name'", "title":"Fix yaml files '$YAML_FILE'"}'
+  curl  -u $USER:$TOKEN -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/opt-nc/$repository_name/pulls -d '{"head":"'yamlfixer/patch/$branch_name/$current_timestamp'","base":"'$branch_name'", "title":"Fix yaml files '$YAML_FILE'"}'
 else
   echo "INFO : all input files either are skipped or successfully pass yamllint strict mode." ;
 fi ;
