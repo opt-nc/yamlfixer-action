524a02106c07459cfe8549863b7cd5a3ab508af6
Michèle <michele.barre@opt.nc>
2022-05-04 15:42:48 +1100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh -l
 
 cd /github/workspace/
-git ls-files '*.yaml' '*.yml' '.yamllint'
+res=$(git ls-files '*.yaml' '*.yml' '.yamllint')
+echo $res
 yamlfixer $OPTIONS --nochange  --diffto /tmp/changes.patch $(git ls-files '*.yaml' '*.yml' '.yamllint' | grep -v ^.github/workflows/) $YAML_FILE
 echo "yamlfixer $OPTIONS --nochange  --diffto /tmp/changes.patch $(git ls-files '*.yaml' '*.yml' '.yamllint' | grep -v ^.github/workflows/) $YAML_FILE"
 
